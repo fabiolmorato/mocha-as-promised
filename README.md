179c21a4990cd66dc07121ae0d2f83c020be051b
Fábio Morato <fabiolmorato@dcc.ufmg.br>
2021-02-22 12:04:39 -0300

    publishing mocha-as-promised lib; README done

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # mocha-as-promised
-A mocha.js and chai wrapper that delivers a promise-based API to get test results instead of only printing.
+A mocha.js and chai wrapper that delivers a promise-based API to get test results instead of only printing. Created for Responde Aí's Bootcamp students use on their own Code LMS platform project.
+
+This library is **UNSAFE** as it uses `eval` to load the code and the test code (check out Usage to understand why).
+
+### Usage
+
+Install the package:
+
+```sh
+npm install @bootcamp-ra/mocha-as-promised
+# or if using yarn
+yarn add @bootcamp-ra/mocha-as-promised
+```
+
+This module exports a single function:
+
+- runTests
+  receives two strings: the code being tested and the test code
+  returns a promise that resolves to the test results
+
+```js
+  import mochaAsPromised from '@bootcamp-ra/mocha-as-promised';
+
+  async function main () {
+    const code = `
+      function testedFunction (param) {
+        return param;
+      }
+    `;
+
+    const testCode = `
+      describe('testedFunction', () => {
+        it('should return the passed parameter', () => {
+          const param = 10;
+
+          const result = testedFunction(param);
+
+          expect(result).to.be(param);
+        });
+      });
+    `;
+
+    const result = await mochaAsPromised.runTests(code, testCode);
+  }
+```
+
+### 
---
+++ index.js
@@ -0,0 +1,148 @@
+import mocha from "mocha/mocha";
+import chai from "chai";
+import transformRemoveConsole from "babel-plugin-transform-remove-console";
+import * as Babel from "@babel/standalone";
+import protect from "@freecodecamp/loop-protect";
+
+const {
+  EVENT_RUN_BEGIN,
+  EVENT_RUN_END,
+  EVENT_TEST_FAIL,
+  EVENT_TEST_PASS,
+  EVENT_SUITE_BEGIN,
+  EVENT_SUITE_END,
+} = mocha.Mocha.Runner.constants;
+
+class ReporterFactory {
+  constructor(...args) {
+    getReporter(...args);
+  }
+  static setResolve(resolve) {
+    this.resolve = resolve;
+  }
+  static getResolve() {
+    return this.resolve;
+  }
+
+  static setReject(reject) {
+    this.reject = reject;
+  }
+
+  static getReject() {
+    return this.reject;
+  }
+}
+
+class Reporter {
+  constructor(resolve, runner) {
+    const stats = runner.stats;
+    this.test = {
+      passed: 0,
+      failed: 0,
+      total: 0,
+      suites: [],
+    };
+    this.currentSuite = [];
+    runner
+      .once(EVENT_RUN_BEGIN, () => {})
+      .on(EVENT_SUITE_BEGIN, () => {})
+      .on(EVENT_SUITE_END, () => {
+        this.test.suites.push(this.currentSuite);
+        this.currentSuite = [];
+      })
+      .on(EVENT_TEST_PASS, (test) => {
+        this.test.total++;
+        this.test.passed++;
+        this.currentSuite.push(test);
+      })
+      .on(EVENT_TEST_FAIL, (test, error) => {
+        this.test.total++;
+        this.test.failed++;
+        test.error = error;
+        this.currentSuite.push(test);
+      })
+      .once(EVENT_RUN_END, () => {
+        resolve(this.test);
+      });
+  }
+}
+
+function getReporter(...args) {
+  return new Reporter(ReporterFactory.getResolve(), ...args);
+}
+
+const timeout = 1500;
+Babel.registerPlugin(
+  "loopProtection",
+  protect(timeout, () => {
+    ReporterFactory.getReject()("timeout");
+  })
+);
+
+Babel.registerPlugin(
+  "babel-plugin-transform-remove-console",
+  transformRemoveConsole
+);
+
+const transform = (source) =>
+  Babel.transform(source, {
+    plugins: ["loopProtection", "babel-plugin-transform-remove-console"],
+  }).code;
+
+let code = "";
+let tests = "";
+
+mocha.setup({
+  ui: "bdd",
+  cleanReferencesAfterRun: false,
+  reporter: ReporterFactory,
+  timeout: 2000,
+});
+
+function run() {
+  return new Promise((resolve, reject) => {
+    ReporterFactory.setResolve(resolve);
+    ReporterFactory.setReject(reject);
+    eval(`
+      const { expect } = chai;
+      ${transform(code)};
+      ${tests};
+      mocha.run();
+    `);
+  });
+}
+
+function reset() {
+  code = "";
+  tests = "";
+  if (mocha.suite) {
+    if (mocha.suite.suites) mocha.suite.suites.splice(0);
+    if (mocha.suite.tests) mocha.suite.tests.splice(0);
+  }
+  if (mocha._previousRunner) {
+    delete mocha._previousRunner;
+  }
+}
+
+function loadCode(c) {
+  code = c;
+}
+
+function loadTests(t) {
+  tests = t;
+}
+
+/**
+ * runTests: runs the tests to the code
+ * @param {string} code code to be tested 
+ * @param {string} tests test code
+ * @returns {Promise<Object>} test results
+ */
+function runTests(code, tests) {
+  reset();
+  loadCode(code);
+  loadTests(tests);
+  return run();
+}
+
+export { runTests };
